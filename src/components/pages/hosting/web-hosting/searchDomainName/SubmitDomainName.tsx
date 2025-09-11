44db55ddeabc7c19a70be9bff63c9ec556c775c6
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2025-09-11 23:23:58 +0600

    Update: SearchDomainName and MoneyBack Section

--- src/components/pages/hosting/web-hosting/searchDomainName/SubmitDomainName.tsx
+++ src/components/pages/hosting/web-hosting/searchDomainName/SubmitDomainName.tsx
@@ -17,18 +17,18 @@ const SubmitDomainName = () => {
 
   return (
     <form
-      className="flex flex-row max-w-xs md:max-w-md lg:max-w-xl mx-auto md:mx-0"
+      className="flex flex-row border border-gray-200 rounded-xl max-w-xs md:max-w-md lg:max-w-xl mx-auto md:mx-0"
       onSubmit={handleSubmit(handleSearch)}
     >
       <input
         type="text"
         {...register("domain", { required: true })}
         placeholder={WebHostingData.domainNameSection.searchPlaceholder}
-        className="flex-1 px-3 py-3 sm:px-4 sm:py-4 bg-[#F3F3F5] text-sm sm:text-base outline-none border-none rounded-t-md sm:rounded-t-none sm:rounded-l-md"
+        className="flex-1 px-3 py-3 sm:px-4 sm:py-4 bg-gray-100 text-lg sm:text-2xl outline-none border-none rounded-t-md sm:rounded-t-none sm:rounded-l-xl font-medium"
       />
       <button
         type="submit"
-        className="flex items-center justify-center cursor-pointer gap-1 sbg-gradient-to-r from-primary-blue to-secondary-blue text-white font-semibold text-sm sm:text-base px-4 sm:px-7 md:px-9 py-3 sm:py-4 rounded-l-none rounded-r-md hover:opacity-90 transition"
+        className="flex items-center justify-center cursor-pointer gap-1 bg-gradient-to-r from-primary-blue to-secondary-blue text-white font-semibold text-lg sm:text-2xl px-4 sm:px-7 md:px-9 py-3 sm:py-4 rounded-l-none rounded-r-xl hover:opacity-90 transition"
       >
         <IoSearch size={20}/> Search
       </button>
