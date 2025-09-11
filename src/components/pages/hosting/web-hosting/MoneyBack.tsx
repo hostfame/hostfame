569ad3bbcf52f5a96ccc16fab49ff72efe7d1e24
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2025-09-11 13:13:40 +0600

    Add: MoneyBack section

--- src/components/pages/hosting/web-hosting/MoneyBack.tsx
+++ src/components/pages/hosting/web-hosting/MoneyBack.tsx
@@ -1,23 +1,7 @@
-import React from 'react';
-import { FaCalendarCheck, FaRegClock } from 'react-icons/fa';
-import { FaArrowRightArrowLeft } from 'react-icons/fa6';
+import { moneyBackFeatures, WebHostingData } from "@/data/hosting.data";
+import React from "react";
 
 const MoneyBack = () => {
-    const features = [
-    {
-      icon: <FaArrowRightArrowLeft className="text-white text-xl rotate-90" />,
-      text: "Easily Upgrade or Downgrade",
-    },
-    {
-      icon: <FaCalendarCheck className="text-white text-xl" />,
-      text: "Get Notified before suspension",
-    },
-    {
-      icon: <FaRegClock className="text-white text-xl" />,
-      text: "Refund process is easy",
-    },
-  ];
-
   return (
     <section className="relative w-full flex flex-col items-center bg-white py-8 md:py-16 px-4 sm:px-8 lg:px-16 text-center shadow-sm overflow-hidden">
       {/* Background Circles */}
@@ -26,23 +10,28 @@ const MoneyBack = () => {
 
       {/* Heading */}
       <h2 className="text-2xl md:text-3xl font-bold text-gray-800">
-        30 day Money-back Guarantee
+        {WebHostingData.moneyBackSection.title}
       </h2>
       <p className="text-gray-500 mt-2 text-sm md:text-base max-w-2xl mx-auto">
-        There is no risk with our 30-day money back guarantee. Stay safe with
-        assurance of refund.
+        {WebHostingData.moneyBackSection.description}
       </p>
 
       {/* Features */}
       <div className="mt-8 flex flex-col md:flex-row items-center justify-center gap-8">
-        {features.map((feature, idx) => (
-          <div key={idx} className="flex items-center gap-3">
-            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 shadow-md">
-              {feature.icon}
+        {moneyBackFeatures.map((feature, idx) => {
+          const Icon = feature.icon;
+
+          return (
+            <div key={idx} className="flex items-center gap-3">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 shadow-md">
+                <span className="text-white text-xl">
+                  <Icon className="w-5 h-5"/>
+                </span>
+              </div>
+              <span className="text-sm sm:text-base font-medium">{feature.text}</span>
             </div>
-            <span className="text-gray-700 font-medium">{feature.text}</span>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
