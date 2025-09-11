01fe6d8ada39367f972a59bc74ac21c99a5d8e0c
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2025-09-11 16:06:24 +0600

    Add: Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/shared/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body
         className={`${urbanist.className} antialiased`}
       >
+        <Navbar />
         {children}
         <Footer />
       </body>
