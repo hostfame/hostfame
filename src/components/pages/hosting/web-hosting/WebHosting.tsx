fc6ca9ffa4ca2aabf8b717cd3f5f89f4062e8b41
Shahriar Haque <shahriarhaquerahat10@gmail.com>
2025-09-11 14:17:55 +0600

    Add: Web Hosting Pricing Section

--- src/components/pages/hosting/web-hosting/WebHosting.tsx
+++ src/components/pages/hosting/web-hosting/WebHosting.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import SearchDomainName from "./searchDomainName/SearchDomainName";
 import SectionWrapper from "@/components/shared/wrappers/SectionWrapper";
 import WebHostingPartners from "./WebHostingPartners";
+import { Pricing } from "./pricing/Pricing";
 
 const WebHosting = () => {
   return (
     <section className="space-y-8 lg:space-y-12 ">
       <SectionWrapper>
         <SearchDomainName />
+        <Pricing />
       </SectionWrapper>
 
-      <WebHostingPartners/>
+      <WebHostingPartners />
     </section>
   );
 };
