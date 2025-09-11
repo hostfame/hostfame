71cfe167eda8093af787078f955893868d66bfc5
ornob90 <oornob49@gmail.com>
2025-09-11 18:16:52 +0600

    fix navbar issue

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   }, [])
 
   return (
-    <nav className="bg-white shadow-lg border-b border-gray-200 sticky top-0 z-50 w-screen">
+    <nav className="bg-white shadow-lg border-b border-gray-200 sticky top-0 z-50">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
