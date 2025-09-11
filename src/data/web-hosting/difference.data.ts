7465eba848d5af863c1cba6a0af4c8c4f5365ea0
Shahriar Haque <shahriar.haque.1011@gmail.com>
2025-09-11 17:37:05 +0600

    Add: difference

---
+++ src/data/web-hosting/difference.data.ts
@@ -0,0 +1,26 @@
+export const differenceData = {
+  title: "What makes Hostnin different from others?",
+  description: "Using our incredible technology, we will help you improve your website's accessibility. With LiteSpeed Server and CloudLinux OS, you can speed up loading times, enhance security, and make your site faster and more reliable.",
+  features: [
+    {
+      icon: "server",
+      title: "Fastest NVMe SSD Server",
+      content: "We use enterprise-grade servers located across multiple locations around the globe. Tune your website with us and take it to the next level.",
+    },
+    {
+      icon: "backup",
+      title: "Daily Multistep Backup",
+      content: "We use a 3-layer backup system to keep your data secure. By generating hundreds of snapshots each month, we ensure that no element is missed.",
+    },
+    {
+      icon: "speed",
+      title: "LiteSpeed Web Server",
+      content: "Website performance is enhanced by combining the LiteSpeed web server with the latest PHP version, making your website faster and more efficient.",
+    },
+    {
+      icon: "lock",
+      title: "Lifetime FREE SSL",
+      content: "Your website will be more secure with an easy-to-install, auto-renewing Let's Encrypt SSL certificate. This ensures that your site remains protected.",
+    },
+  ],
+};
