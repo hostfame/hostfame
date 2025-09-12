a08f753e7ca156b6e056203acafd641b45871fa9
ornob90 <oornob49@gmail.com>
2025-09-12 15:01:25 +0600

    making FAQ shared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import WebHosting from "@/components/pages/hosting/web-hosting/WebHosting";
-
-
 export const dynamic = "force-static";
 export const revalidate = 3600;
 
 export default function Home() {
-  return <WebHosting />;
+  return <></>;
 }
