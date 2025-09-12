6717ef34fbf28c69fb6dacd249891ef9fe2415ab
Shahriar Haque <shahriar.haque.1011@gmail.com>
2025-09-12 16:23:57 +0600

    Update: pricing

--- src/components/pages/hosting/web-hosting/pricing/Pricing.tsx
+++ src/components/pages/hosting/web-hosting/pricing/Pricing.tsx
@@ -38,7 +38,7 @@ export function Pricing() {
           yearlyDiscount={pricingData.yearlyDiscount}
         />
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-14 lg:gap-6 max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-14 lg:gap-6 max-w-7xl mx-auto pt-18">
           {pricingData.plans.map((plan, index) => (
             <div
               key={plan.id}
