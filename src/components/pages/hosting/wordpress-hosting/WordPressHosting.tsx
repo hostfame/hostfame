ef85091e1a91581a2d723ae9a913c606476e6dcf
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2025-09-12 16:21:11 +0600

    Add: ManageHostingHighlights section

--- src/components/pages/hosting/wordpress-hosting/WordPressHosting.tsx
+++ src/components/pages/hosting/wordpress-hosting/WordPressHosting.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ManagedWordPressHosting from "./ManagedWordPressHosting";
 import SectionWrapper from "@/components/shared/wrappers/SectionWrapper";
+import { wordpressHostingData } from "@/types/wordpress-hosting/wordpress-hosting.data";
+import ManagedHostingHighlights from "@/components/shared/sections/ManagedHostingHighlights";
 
 const WordPressHosting = () => {
   return (
     <section>
       <SectionWrapper>
-        <ManagedWordPressHosting />
+        <ManagedHostingHighlights data={wordpressHostingData}/>
       </SectionWrapper>
     </section>
   );
