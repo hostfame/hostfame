4c952a94c3232850f85270cd9e205c3dc6071e28
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2025-09-13 13:02:12 +0600

    Add: DifferentHostingPlans section

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import DifferentHostingPlans from "@/components/shared/sections/DifferentHostingPlans";
+import { differentHostingPlansData } from "@/data/home.data";
+import React from "react";
 
 const Home = () => {
   return (
     <section>
-        
+      <DifferentHostingPlans data={differentHostingPlansData}/>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
