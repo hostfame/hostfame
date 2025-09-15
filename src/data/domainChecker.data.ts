0d0201f27551dbb6a73fbca595539ab5c9ef0fbb
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2025-09-15 19:58:42 +0600

    Add: WhyChooseHostfame Section

---
+++ src/data/domainChecker.data.ts
@@ -0,0 +1,30 @@
+import { FaLock } from "react-icons/fa";
+import { FaFilePen, FaMoneyCheckDollar } from "react-icons/fa6";
+import { MdOutlineSupportAgent } from "react-icons/md";
+
+export const DomainCheckerWhyChooseHostfameData = {
+  title: "Why Choose Hostfame?",
+  description: "We make hosting simple, transparent, and truly human—so you focus on what matters most.",
+  cards : [
+    {
+      title: "Instant Registration",
+      desc: "Get your perfect domain instantly—fast, secure, and hassle-free.",
+      icon: FaFilePen,
+    },
+    {
+      title: "Free WHOIS Privacy",
+      desc: "We include free WHOIS privacy protection (where supported) to keep your data safe.",
+      icon: FaLock,
+    },
+    {
+      title: "Transparent Pricing",
+      desc: "No hidden charges, no confusing upsells. Always know exactly what you’re paying.",
+      icon: FaMoneyCheckDollar,
+    },
+    {
+      title: "Real Human Support",
+      desc: "Talk to a real person anytime via chat, ticket, or WhatsApp—no bots, ever.",
+      icon: MdOutlineSupportAgent,
+    },
+  ]
+}
