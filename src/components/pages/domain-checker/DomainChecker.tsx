9f440c96eef2047cc4f07e6bf7fc8c228781a4ee
Shahriar Haque <shahriar.haque.1011@gmail.com>
2025-09-15 20:47:35 +0600

    Update: Banners

--- src/components/pages/domain-checker/DomainChecker.tsx
+++ src/components/pages/domain-checker/DomainChecker.tsx
@@ -2,16 +2,18 @@ import SectionWrapper from '@/components/shared/wrappers/SectionWrapper';
 import React from 'react';
 import DomainPricing from '../../shared/sections/DomainPricing';
 import WhyChooseHostfame from './WhyChooseHostfame';
+import DomainCheckerBanner from '@/components/shared/sections/DomainCheckerBanner';
 
 const DomainChecker = () => {
   return (
     <div>
+      <DomainCheckerBanner />
       <SectionWrapper className="my-12 md:my-16">
         <DomainPricing />
       </SectionWrapper>
 
       <SectionWrapper className="my-12 md:my-16">
-        <WhyChooseHostfame/>
+        <WhyChooseHostfame />
       </SectionWrapper>
     </div>
   );
