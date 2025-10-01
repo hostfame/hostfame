b5fe3c7ff7b774004d2f0416e24ac8d52cf3869d
Shahriar Haque <shahriar.haque.1011@gmail.com>
2025-10-01 14:27:20 +0600

    Update: top offer

--- src/components/shared/Offer.tsx
+++ src/components/shared/Offer.tsx
@@ -8,8 +8,8 @@ const Offer = () => {
 
   const text =
     countryCode === "BD"
-      ? "Only ৳200/mo Hosting + Free .COM"
-      : "Only $1.49/mo Hosting + Free .COM";
+      ? "Only ৳999/year Hosting with 24/7 Support"
+      : "Only $9.99/year Hosting with 24/7 Support";
 
   return (
     <section className="flex font-bold  z-40 bg-offer-bg p-2 text-offer-text  w-full justify-center items-center gap-x-1 text-xs">
